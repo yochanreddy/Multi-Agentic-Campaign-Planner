b4813932387a93d5e958dadcfaa5ed25448daf00
Parth Patel <parthpatel643@gmail.com>
2025-03-06 21:24:30 +0530

    reformatted the codebase and removed dead code

---
+++ campaign_planner/agents/audience_segment_analyzer/input.py
@@ -0,0 +1,29 @@
+from typing import Any, Dict
+
+from campaign_planner.agents.base import BaseInputNode
+from langchain_core.runnables.config import RunnableConfig
+from campaign_planner.utils import get_module_logger
+from pydantic import BaseModel, Field
+
+logger = get_module_logger()
+
+
+class InputSchema(BaseModel):
+    brand_name: str = Field(..., description="Name of Brand")
+    brand_description: str = Field(..., description="Description of Brand")
+    campaign_objective: str = Field(..., description="Objective of the campaign")
+    industry: str = Field(..., description="type of industry")
+
+    class Config:
+        extra = "ignore"
+
+
+class InputNode(BaseInputNode):
+    def validate_and_parse(
+        self, state: Dict[str, Any], config: RunnableConfig
+    ) -> Dict[str, Any]:
+        """Validate and parse input state"""
+        logger.debug(f"{config['configurable']['thread_id']} start")
+        input_data = InputSchema.model_validate(state)
+        logger.debug(f"{config['configurable']['thread_id']} finish")
+        return input_data.model_dump()
---
+++ campaign_planner/agents/marketing_budget_allocator/output.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel, Field
+from typing import Any, Dict, Literal
+
+from campaign_planner.agents.base import BaseOutputNode
+from langchain_core.runnables.config import RunnableConfig
+from campaign_planner.utils import get_module_logger
+from langchain.output_parsers import PydanticOutputParser
+
+logger = get_module_logger()
+ChannelType = Literal["Meta", "Google", "LinkedIn", "TikTok"]
+
+
+class OutputSchema(BaseModel):
+    total_budget: float = Field(
+        description="The total daily budget predicted to run a campaign based on the previous outputs"
+    )
+    channel_budget_allocation: Dict[ChannelType, float] = Field(
+        description="A dictionary with the recommended channel names as keys and their respective daily budget allocations in INR"
+    )
+
+    class Config:
+        extra = "ignore"
+
+
+class OutputNode(BaseOutputNode):
+    output_parser = PydanticOutputParser(pydantic_object=OutputSchema)
+
+    def __init__(self):
+        super().__init__()
+
+    def format_output(
+        self, state: Dict[str, Any], config: RunnableConfig
+    ) -> OutputSchema:
+        """Format the output state"""
+        logger.debug(f"{config['configurable']['thread_id']} start")
+        last_message = state["messages"][-1]
+        output_data: OutputSchema = self.output_parser.invoke(last_message)
+        logger.debug(f"{config['configurable']['thread_id']} finish")
+        return output_data.model_dump()
